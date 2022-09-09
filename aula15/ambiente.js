189d9f50fdce0b75935fb158d2aedeabff17f918
Bruno Strelow <bruno.strelow@hotmail.com>
2022-09-09 10:25:20 -0300

    Adiciona conteúdo aula 15 - Array

---
+++ aula15/ambiente.js
@@ -0,0 +1,16 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+// console.log(num.length)
+// num.sort()
+
+
+// for (let pos = 0; pos < num.length; pos++){
+//     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+// }
+
+// for(let pos in num){
+//     // console.log(num[pos])
+//     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+// }
+let pos = num.indexOf(9)
+console.log(pos)
